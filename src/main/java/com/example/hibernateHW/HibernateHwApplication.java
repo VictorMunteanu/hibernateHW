e0a5a5572a8aaa217efd908f1663e5de6fe90eed
Munteanu Victor <munteanuvictor46@gmail.com>
2021-10-04 18:51:34 +0300

    Initial commit

---
+++ src/main/java/com/example/hibernateHW/HibernateHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibernateHW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hibernateHW/HibernateHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hibernateHW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
