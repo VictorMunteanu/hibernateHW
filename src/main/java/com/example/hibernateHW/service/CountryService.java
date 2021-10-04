0ec7c63ebcfc061caf0af39b0c33adf46ce7b508
Munteanu Victor <munteanuvictor46@gmail.com>
2021-10-04 19:54:41 +0300

    add save method

--- src/main/java/com/example/hibernateHW/service/CountryService.java
+++ src/main/java/com/example/hibernateHW/service/CountryService.java
@@ -1,4 +1,7 @@
 package com.example.hibernateHW.service;
 
+import com.example.hibernateHW.model.Country;
+
 public interface CountryService {
+    Country save(Country country);
 }
--- src/main/java/com/example/hibernateHW/service/impl/CoutryServiceImpl.java
+++ src/main/java/com/example/hibernateHW/service/impl/CoutryServiceImpl.java
@@ -1,4 +1,22 @@
 package com.example.hibernateHW.service.impl;
 
-public class CoutryServiceImpl {
+import com.example.hibernateHW.model.Country;
+import com.example.hibernateHW.repository.CountryRepository;
+import com.example.hibernateHW.service.CountryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CoutryServiceImpl implements CountryService {
+    CountryRepository countryRepository;
+
+    public CountryServiceImpl(CountryRepository countryRepository) {
+        super();
+        this.countryRepository = countryRepository;
+    }
+
+    @Override
+    public Country save(Country country) {
+        return countryRepository.save(country);
+    }
+
 }
