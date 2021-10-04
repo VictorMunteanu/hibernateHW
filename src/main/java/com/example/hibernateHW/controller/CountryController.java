81d8d5de17238cddfb2f6b220c12569fadf3f090
Munteanu Victor <munteanuvictor46@gmail.com>
2021-10-04 19:04:47 +0300

    create CountryController

---
+++ src/main/java/com/example/hibernateHW/controller/CountryController.java
@@ -0,0 +1,10 @@
+package com.example.hibernateHW.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryController {
+
+}
